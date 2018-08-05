a318eeae6ba5d2621f4d959140ca5271505bf97a
proclnas <proclnas@gmail.com>
2018-08-04 21:48:56 -0300

    Migrations
    - Created address migration

---
+++ database/migrations/2018_08_05_003552_create_address_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('lograudoro', 50); 
+            $table->string('numero', 6);
+            $table->string('cep', 7);
+            $table->string('cidade', 50);
+            $table->string('complemento', 50);
+            $table->string('bairro', 50);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
